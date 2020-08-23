6b40a60e19602641fef4415d0abf28a420927a6e
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-08-23 17:31:25 +0900

    navbar manu worked

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "../../stylesheets/application.sass"
+import "../src/navbar_burger"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
