6e240e91cdddce60bd461fa26eff27313c9c8027
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-08-30 17:31:29 +0900

    新着店舗と人気店舗をTopに表示

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -1,5 +1,7 @@
 class WebController < ApplicationController
   def index
+    @new_restaurants = Restaurant.new_restaurants
+    @popular_restaurants = Restaurant.popular_restaurants
   end
 
   def about
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,12 @@
 class Restaurant < ApplicationRecord
     has_many_attached :images
+
+
+    scope :new_restaurants, -> {
+        last(4)
+    }
+
+    scope :popular_restaurants, -> {
+        last(4)
+    }
 end
