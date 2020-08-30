14dfa5979a82c753a8b33622fedb49679dff334c
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-08-30 16:21:49 +0900

    ルーティングにコメント追加など

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  namespace :dashboard do
-    root 'top#index'
-    resources :restaurants
-  end
+  # ユーザーのルーティング
   root 'web#index'
   get '/about', to: 'web#about'
   get '/contact', to: 'web#contact'
   resources :restaurants, only: [:index, :show]
+
+  # 管理画面のルーティング
+  namespace :dashboard do
+    root 'top#index'
+    resources :restaurants
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
