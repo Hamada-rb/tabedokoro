614d733b38b1f450282c0397e6b617dc1af1eee8
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-08-30 20:40:52 +0900

    リダイレクト先の修正

--- app/controllers/dashboard/application_controller.rb
+++ app/controllers/dashboard/application_controller.rb
@@ -1,4 +1,10 @@
 class Dashboard::ApplicationController < ActionController::Base
-    before_action :authenticate_admin!
+    before_action :auth_admin!
     layout 'dashboard'
+
+    private
+
+    def auth_admin!
+        redirect_to :root if !admin_signed_in?
+    end
 end
